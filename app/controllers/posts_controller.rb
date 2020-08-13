59c22eedc102acb40e3b6ba952458f6de0024184
mariapaz <mpazcarvacho@gmail.com>
2020-08-13 03:36:48 -0400

    autenticacion en dashboard y formulario

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,12 @@
 class PostsController < ApplicationController
+  http_basic_authenticate_with name:"desafiovamoscontodo", password:"XAHTJEAS23123%23", only: :dashboard
+
   def index
   end
 
   def dashboard
+  end
+
+  def create
   end
 end
